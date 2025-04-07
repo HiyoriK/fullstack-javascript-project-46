c9c734f7f61b54b4a19ff50391653baa1d81be7d
Alyona Stepanova <kiriyuuhiyori@gmail.com>
2025-04-07 20:14:03 +0300

    third step without extensions

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import parse from './parser.js';
+
+const gendiff = (filepath1, filepath2) => {
+    const fullpath1 = path.resolve(filepath1);
+    const fullpath2 = path.resolve(filepath2);
+
+    const ext1 = path.extname(filepath1);
+    const ext2 = path.extname(filepath2);
+
+    const content1 = fs.readFileSync(filepath1, 'utf-8');
+    const content2 = fs.readFileSync(filepath2, 'utf-8');
+
+    const obj1 = parse(content1);
+    const obj2 = parse(content2);
+
+    console.log(obj1, obj2);
+}
+
+export default gendiff;
